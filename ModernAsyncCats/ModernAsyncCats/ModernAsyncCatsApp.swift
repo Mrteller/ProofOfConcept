787f9cae47a69f753e94f0d2a3260ce941207f91
Mrteller <39244601+Mrteller@users.noreply.github.com>
2022-04-07 12:17:30 +0300

    Modern Async Cats

---
+++ ModernAsyncCats/ModernAsyncCats/ModernAsyncCatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernAsyncCatsApp.swift
+//  ModernAsyncCats
+//
+//  Created by Paul on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ModernAsyncCatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
