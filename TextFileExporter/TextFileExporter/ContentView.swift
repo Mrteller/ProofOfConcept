8bd668a4663f8848f4d9a977c8b088bf139c3afc
Mrteller <39244601+Mrteller@users.noreply.github.com>
2022-07-28 12:37:00 +0300

    Added share sheet

--- TextFileExporter/TextFileExporter/ContentView.swift
+++ TextFileExporter/TextFileExporter/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var isShowingExporter = false
     @State private var textToSave = ""
+    @State private var isShowingShareSheet = false
     var body: some View {
         VStack {
             Text("Save txt example")
@@ -21,6 +22,25 @@ struct ContentView: View {
                 Label("Export", systemImage: "square.and.arrow.down")
             }
 
+            Button {
+                let url = getDocumentsDirectory().appendingPathComponent("hardcoded.txt")
+                do {
+                    try textToSave.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            } label: {
+                Label("Export to documentDirectory", systemImage: "square.and.arrow.down")
+            }
+            
+            Button {
+                isShowingShareSheet = true
+            } label: {
+                Label("Share", systemImage: "square.and.arrow.down")
+            }
+
         }
         .fileExporter(isPresented: $isShowingExporter, document: TextFile(initialText: textToSave), contentType: .plainText, defaultFilename: "Test") { result in
             switch result {
@@ -30,6 +50,17 @@ struct ContentView: View {
                 print(error.localizedDescription)
             }
         }
+        .sheet(isPresented: $isShowingShareSheet) {
+            ShareSheet(activityItems: [textToSave])
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        // just send back the first one, which ought to be the only one
+        return paths[0]
     }
 }
 
