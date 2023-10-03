c381a853a97b274a99c66e50355bae20f62a7e15
Mrteller <39244601+Mrteller@users.noreply.github.com>
2023-10-03 15:36:40 +0300

    CHORE: Added sample for View lifecycle experiments.

---
+++ OpaqueTypesAndBoxedProtocolTypes.playground/Contents.swift
@@ -0,0 +1,117 @@
+import UIKit
+
+protocol Container<Item> {
+    associatedtype Item
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+}
+extension Array: Container { }
+
+func makeProtocolContainer<T>(item: T) -> any Container<T> {
+    return [item]
+}
+
+// Boxed protocol type with primary associated type
+let protocolContainer = makeProtocolContainer(item: 11)
+let eleven = protocolContainer[0]
+print(type(of: eleven))
+
+// Opaque type also works as before
+func makeOpaqueContainer<T>(item: T) -> some Container {
+    return [item]
+}
+let opaqueContainer = makeOpaqueContainer(item: 12)
+let twelve = opaqueContainer[0]
+print(type(of: twelve))
+
+// Code from Swift [doc](https://docs.swift.org/swift-book/documentation/the-swift-programming-language/opaquetypes)
+
+
+protocol Shape {
+    func draw() -> String
+}
+
+
+struct Triangle: Shape {
+    var size: Int
+    func draw() -> String {
+        var result: [String] = []
+        for length in 1...size {
+            result.append(String(repeating: "*", count: length))
+        }
+        return result.joined(separator: "\n")
+    }
+}
+let smallTriangle = Triangle(size: 3)
+print(smallTriangle.draw())
+// *
+// **
+// ***
+
+struct JoinedShape<T: Shape, U: Shape>: Shape {
+    var top: T
+    var bottom: U
+    func draw() -> String {
+        return top.draw() + "\n" + bottom.draw()
+    }
+}
+let joinedTriangles = JoinedShape(top: smallTriangle, bottom: flippedTriangle)
+print(joinedTriangles.draw())
+// *
+// **
+// ***
+// ***
+// **
+// *
+
+struct Square: Shape {
+    var size: Int
+    func draw() -> String {
+        let line = String(repeating: "*", count: size)
+        let result = Array<String>(repeating: line, count: size)
+        return result.joined(separator: "\n")
+    }
+}
+
+
+func makeTrapezoid() -> some Shape {
+    let top = Triangle(size: 2)
+    let middle = Square(size: 2)
+    let bottom = FlippedShape(shape: top)
+    let trapezoid = JoinedShape(
+        top: top,
+        bottom: JoinedShape(top: middle, bottom: bottom)
+    )
+    return trapezoid
+}
+let trapezoid = makeTrapezoid()
+print(trapezoid.draw())
+// *
+// **
+// **
+// **
+// **
+// *
+
+struct FlippedShape<T: Shape>: Shape {
+    var shape: T
+    func draw() -> String {
+        let lines = shape.draw().split(separator: "\n")
+        return lines.reversed().joined(separator: "\n")
+    }
+}
+let flippedTriangle = FlippedShape(shape: smallTriangle)
+print(flippedTriangle.draw())
+// ***
+// **
+// *
+
+func protoFlip<T: Shape>(_ shape: T) -> any Shape {
+    if shape is Square {
+        return shape
+    }
+    
+    
+    return FlippedShape(shape: shape)
+}
+
---
+++ ViewLifecyleTest/ViewLifecyleTest/ViewLifecyleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewLifecyleTestApp.swift
+//  ViewLifecyleTest
+//
+//  Created by Paul on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewLifecyleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
