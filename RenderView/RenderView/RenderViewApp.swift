09587c968ed6b33eab21a63f28d5653103e56a9c
Mrteller <39244601+Mrteller@users.noreply.github.com>
2023-10-06 19:10:23 +0300

    Experiment with `ImageRenderer`.

---
+++ RenderView/RenderView/RenderViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenderViewApp.swift
+//  RenderView
+//
+//  Created by Paul on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct RenderViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
